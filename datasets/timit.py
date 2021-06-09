d79cee461f4fa1dbcd634a5c70a74c11630b96ac
AndreyBocharnikov <a-bocharnikov@bk.ru>
2021-06-09 21:05:19 +0300

    eval added

--- datasets/timit.py
+++ datasets/timit.py
@@ -7,8 +7,7 @@ import torchaudio
 
 
 class TimitTrain(Dataset):
-    def __init__(self, path, chunk_len, sr):
-        self.sr = sr
+    def __init__(self, path, chunk_len):
         self.chunk_len = chunk_len
         self.path = path
         self.labels = np.load(os.path.join(path, 'labels.npy'), allow_pickle=True).item()
@@ -22,12 +21,43 @@ class TimitTrain(Dataset):
         wav_path = self.train_samples[idx]
         label = self.labels[wav_path]
         wav, sr = torchaudio.load(os.path.join(self.path, wav_path))
-        assert sr == self.sr
-        n_sample_chunk = int(self.sr * self.chunk_len)
-        if n_sample_chunk > len(wav[0]):
-            print(wav_path)
-            assert False
-        start = np.random.randint(len(wav[0]) - n_sample_chunk)
-        chunk = wav[0, start:start + n_sample_chunk]
+        start = np.random.randint(len(wav[0]) - self.chunk_len)
+        chunk = wav[0, start:start + self.chunk_len]
         volume_gain = 0.4 * torch.rand(1) + 0.8
         return torch.unsqueeze(chunk, 0) * volume_gain, label
+
+
+class TimitVal(Dataset):
+    def __init__(self, path, chunk_len, chunk_shift):
+        self.cur_wav_id, self.cur_chunk = 0, 0
+        self.path, self.chunk_len, self.chunk_shift = path, chunk_len, chunk_shift
+        self.labels = np.load(os.path.join(path, 'labels.npy'), allow_pickle=True).item()
+        with open(os.path.join(path, 'test.scp')) as f:
+            self.test_samples = f.read().splitlines()
+        self.wav_path = os.path.join(self.path, self.test_samples[self.cur_wav_id])
+        self.cur_wav, _ = torchaudio.load(self.wav_path)
+
+    def __len__(self):
+        return len(self.test_samples)
+
+    """
+    def __getitem__(self, _):
+        label = self.labels[self.wav_path]
+        chunk = self.cur_wav[0, self.cur_chunk:self.chunk_len]
+        self.cur_chunk += self.chunk_shift
+        chunk_id = self.cur_wav_id
+        if self.cur_chunk >= len(self.cur_wav[0]):
+            self.cur_chunk = 0
+            self.cur_wav_id += 1
+            self.wav_path = os.path.join(self.path, self.test_samples[self.cur_wav_id])
+            self.cur_wav, _ = torchaudio.load(self.wav_path)
+        return chunk, label, chunk_id
+    """
+    def __getitem__(self, idx):
+        wav_path = self.test_samples[idx]
+        label = self.labels[wav_path]
+        wav, sr = torchaudio.load(os.path.join(self.path, wav_path))
+        chunks = []
+        for start in range(0, len(wav[0]) - self.chunk_len + 1, self.chunk_shift):
+            chunks.append(wav[0, start:start + self.chunk_len])
+        return torch.unsqueeze(torch.stack(chunks), dim=1), label, len(chunks)
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import torch.optim
 import wandb # sudo apt install libpython3.7-dev python3.7 -m pip install wandb
 import numpy as np
 
-from datasets.timit import TimitTrain
+from datasets.timit import TimitTrain, TimitVal
 from model import SincNet
 from utils import NestedNamespace
 
@@ -17,17 +17,20 @@ with open('cfg.yaml') as config:
 #wandb.init(project='SincNet')
 #wandb.config = params
 
-wav_len = int(params.sample_rate * params.chunk_len)
-dataset = TimitTrain(params.data.timit.path, chunk_len=params.chunk_len, sr=params.sample_rate)
-dataloader = DataLoader(dataset, batch_size=params.batch_size, shuffle=True, drop_last=True, num_workers=2)
+chunk_len = int(params.sample_rate * params.chunk_len_ratio)
+chunk_shift = int(params.sample_rate * params.chunk_shift_ratio)
+dataset_train = TimitTrain(params.data.timit.path, chunk_len=chunk_len)
+dataset_val = TimitVal(params.data.timit.path, chunk_len, chunk_shift)
+dataloader = DataLoader(dataset_train, batch_size=params.batch_size, shuffle=True, drop_last=True, num_workers=2)
 
-sinc_net = SincNet(wav_len, params.data.timit.n_classes)
+sinc_net = SincNet(chunk_len, params.data.timit.n_classes)
 sinc_net = sinc_net.to(params.device)
 optim = torch.optim.RMSprop(sinc_net.parameters(), lr=params.lr, alpha=0.95, eps=1e-8)
 criterion = nn.CrossEntropyLoss()
 
 for i in range(params.n_epochs):
     accuracy, losses = [], []
+    sinc_net.train()
     for j, batch in enumerate(dataloader):
         optim.zero_grad()
         chunks, labels = batch
@@ -38,11 +41,27 @@ for i in range(params.n_epochs):
         optim.step()
     
         losses.append(loss.item())
-        accuracy.append(torch.mean((torch.argmax(logits, dim=1) == labels).float()).item())
-    if i % 400 == 0:
-      print("mean loss", np.mean(losses))
-      print("mean accuracy", np.mean(accuracy))
-      print()
+        accuracy.append(torch.mean((torch.argmax(logits, dim=1) == labels).float()).item()) # TODO copypaste
+
+    if i % 1 == 0:
+        sinc_net.eval()
+        with torch.no_grad():
+            chunks_accuracy = []
+            losses_test = []
+            wavs_accuracy = 0
+            for chunks, label, n_chunks in dataset_val:
+                chunks = chunks.to(params.device)
+                logits = sinc_net(chunks)
+                loss = criterion(logits, torch.Tensor([label] * n_chunks).long())
+                losses_test.append(loss.item())
+                chunks_accuracy.append(torch.mean((torch.argmax(logits, dim=1) == label).float()).item())
+                wavs_accuracy += torch.argmax(logits.sum(dim=0)) == label
+
+            print("mean train loss", np.mean(losses))
+            print("mean train accuracy", np.mean(accuracy))
+            print("mean test loss", np.mean(losses_test))
+            print("mean test chunk accuracy", np.mean(chunks_accuracy))
+            print("mean test wav accuracy", wavs_accuracy / len(dataset_val))
     #wandb.log({'accuracy': np.mean(accuracy), 'loss': np.mean(losses)})
 
 
