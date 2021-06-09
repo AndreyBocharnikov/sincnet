c2d50de6c8302ea7fab87eb475283e4e02331d09
AndreyBocharnikov <a-bocharnikov@bk.ru>
2021-06-09 15:24:32 +0000

    baseline learning!

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import yaml
+import time
 
 from torch.utils.data import DataLoader
 import torch.nn as nn
@@ -17,10 +18,11 @@ with open('cfg.yaml') as config:
 #wandb.config = params
 
 wav_len = int(params.sample_rate * params.chunk_len)
-dataset = TimitTrain('datasets/timit', chunk_len=params.chunk_len, sr=params.sample_rate)
-dataloader = DataLoader(dataset, batch_size=params.batch_size, shuffle=True)
+dataset = TimitTrain(params.data.timit.path, chunk_len=params.chunk_len, sr=params.sample_rate)
+dataloader = DataLoader(dataset, batch_size=params.batch_size, shuffle=True, drop_last=True, num_workers=2)
 
 sinc_net = SincNet(wav_len, params.data.timit.n_classes)
+sinc_net = sinc_net.to(params.device)
 optim = torch.optim.RMSprop(sinc_net.parameters(), lr=params.lr, alpha=0.95, eps=1e-8)
 criterion = nn.CrossEntropyLoss()
 
@@ -29,13 +31,18 @@ for i in range(params.n_epochs):
     for j, batch in enumerate(dataloader):
         optim.zero_grad()
         chunks, labels = batch
+        chunks, labels = chunks.to(params.device), labels.to(params.device)
         logits = sinc_net(chunks)
         loss = criterion(logits, labels)
         loss.backward()
         optim.step()
-
+    
         losses.append(loss.item())
         accuracy.append(torch.mean((torch.argmax(logits, dim=1) == labels).float()).item())
+    if i % 400 == 0:
+      print("mean loss", np.mean(losses))
+      print("mean accuracy", np.mean(accuracy))
+      print()
     #wandb.log({'accuracy': np.mean(accuracy), 'loss': np.mean(losses)})
 
 
