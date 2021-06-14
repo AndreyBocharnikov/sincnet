4d1633840e933866f855641ec1b3d6c8fc15a47d
AndreyBocharnikov <a-bocharnikov@bk.ru>
2021-06-15 01:13:26 +0300

    docker added

--- bins/run_container.sh
+++ bins/run_container.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-image_name=sincnet
-project_name=sincnet
-username=$(whoami)
-container_name=${username}-${image_name}
-
-docker stop "${container_name}"
-docker rm "${container_name}"
-
 docker run -it \
     --detach \
-    086bb8a82fdf
+	--name gpu \
+	--gpus all \
+    sincnet
